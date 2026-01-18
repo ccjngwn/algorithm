1eec1f4eeebba68ecd4522080a5ef57ddfea9f2a
ccjngwn <114287020+ccjngwn@users.noreply.github.com>
2026-01-19 01:33:44 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 124 ms, Memory: 16072 KB -BaekjoonHub

---
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/**
+*/
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = Integer.parseInt(st.nextToken());
+        int sub = Integer.parseInt(st.nextToken());
+
+        if ((sum + sub) % 2 != 0 || sum < sub) {
+            System.out.println(-1);
+        } else {
+            System.out.println(((sum + sub) / 2) + " " + (((sum + sub) / 2) - sub));
+        }
+    }
+}
