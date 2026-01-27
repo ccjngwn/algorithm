f3f0bdabae816662c8278d3544f80d1f3aec15ae
ccjngwn <114287020+ccjngwn@users.noreply.github.com>
2026-01-28 01:04:01 +0900

    [Bronze II] Title: 문어, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/21313. 문어/문어.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1 && n % 2 == 1) {
+                sb.append("3"); 
+            } else {
+                sb.append((i % 2) + 1).append(" ");
+            }
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
