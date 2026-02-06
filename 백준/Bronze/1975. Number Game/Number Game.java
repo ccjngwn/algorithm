9c3b594719b426698e56b9e02985b630fb7151ce
ccjngwn <114287020+ccjngwn@users.noreply.github.com>
2026-02-06 18:19:15 +0900

    [Bronze I] Title: Number Game, Time: 792 ms, Memory: 33596 KB -BaekjoonHub

---
+++ 백준/Bronze/1975. Number Game/Number Game.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/**
+*/
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int count = 0;
+            int N = Integer.parseInt(br.readLine());
+
+            for (int j = 2; j <= N; j++) {
+                int val = N;
+                while (true) {
+                    if (val % j != 0) {
+                        break;
+                    }
+
+                    count++;
+                    val /= j;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
