c63d9f0ee3caa5e568f5ed5fcba8f93aca8c72d9
ccjngwn <114287020+ccjngwn@users.noreply.github.com>
2026-02-15 21:39:10 +0900

    [Silver II] Title: 이항 계수 2, Time: 112 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+
+    static int[][] dp;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        dp = new int[N + 1][K + 1];
+
+        System.out.println(BC(N, K));
+    }
+
+    public static int BC(int N, int K) {
+        if (dp[N][K] > 0) {
+            return dp[N][K];
+        }
+
+        if (K == 0 || K == N) {
+            return dp[N][K] = 1;
+        }
+
+        return dp[N][K] = (BC(N - 1, K - 1) + BC(N - 1, K)) % 10007;
+    }
+}
