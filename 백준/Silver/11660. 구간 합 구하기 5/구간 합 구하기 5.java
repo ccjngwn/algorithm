ae4a8bb893f08ea37b0e5d527344117d23b27e97
ccjngwn <114287020+ccjngwn@users.noreply.github.com>
2026-03-18 14:41:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1348 ms, Memory: 128804 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/*
+입력받아서 2차원 배열 만들기
+2차원 배열의 누적합 계산하기 - prefix_sum[x, y] = arr[x - 1, y - 1] + prefix_sum[x - 1, y] + prefix_sum[x, y - 1] - prefix_sum[x - 1, y - 1]
+m번 반복 돌면서 구간합 계산하기 - prefix_sum[x2, y2] - prefix_sum[x1 - 1, y2] - prefix_sum[x2, y1 - 1] + prefix_sum[x1 - 1, y1 - 1]
+*/
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] prefix_sum = new int[n + 1][n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                prefix_sum[i][j] = arr[i - 1][j - 1] + prefix_sum[i - 1][j] + prefix_sum[i][j - 1] - prefix_sum[i - 1][j - 1];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = prefix_sum[x2][y2] - prefix_sum[x1 - 1][y2] - prefix_sum[x2][y1 - 1] + prefix_sum[x1 - 1][y1 - 1];
+            System.out.println(sum);
+        }
+    }
+}
