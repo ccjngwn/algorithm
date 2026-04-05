2ef0c75c3385c0970438ea8e6ff8c56dd8a9e46a
ccjngwn <114287020+ccjngwn@users.noreply.github.com>
2026-04-06 00:18:49 +0900

    [level 2] Title: 주식가격, Time: 32.01 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -6,15 +6,31 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
+        // for (int i = 0; i < prices.length; i++) {
+        //     int cur_stock = prices[i];
+        //     for (int j = i + 1; j < prices.length; j++) {
+        //         answer[i]++;
+        //         int comp_stock = prices[j];
+        //         if (cur_stock > comp_stock) {
+        //             break;
+        //         }
+        //     }
+        // }
+        
+        Deque<Integer> deq = new ArrayDeque<>();
+        
         for (int i = 0; i < prices.length; i++) {
-            int cur_stock = prices[i];
-            for (int j = i + 1; j < prices.length; j++) {
-                answer[i]++;
-                int comp_stock = prices[j];
-                if (cur_stock > comp_stock) {
-                    break;
-                }
+            while (!deq.isEmpty() && prices[deq.peekLast()] > prices[i]) {
+                int cur = deq.removeLast();
+                answer[cur] = i - cur;
             }
+            
+            deq.addLast(i);
+        }
+        
+        while (!deq.isEmpty()) {
+            int cur = deq.removeLast();
+            answer[cur] = prices.length - cur - 1;
         }
         
         return answer;
