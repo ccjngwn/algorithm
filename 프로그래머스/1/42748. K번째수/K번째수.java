065465f7cbbd5a85b3ad94ead03705a70f4cddcd
ccjngwn <114287020+ccjngwn@users.noreply.github.com>
2026-05-04 22:07:03 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0; i < commands.length; i++) {
+            int[] tmp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(tmp);
+            answer[i] = tmp[commands[i][2] - 1];
+        }
+        
+        return answer;
+    }
+}
